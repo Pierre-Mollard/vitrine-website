f900bc029903e2adc38928e20abf87c371dfeda4
Pierre Mollard <58636597+Pierre-Mollard@users.noreply.github.com>
2025-03-28 15:26:10 +0100

    feat: added nuxt framework with integrated vite and added tailwinds

--- my-website/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-    base: '/vitrine-website/',
-    plugins: [
-        tailwindcss(),
-    ],
-})
---
+++ nuxt-website/nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  alias: { "@": resolve(__dirname, "/"), },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    base: '/vitrine-website/',
+    plugins: [
+      tailwindcss(),
+    ],
+  }
+})
